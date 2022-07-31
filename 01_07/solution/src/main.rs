6e481579541361ede12d179a23007c7e4122365c
Marcus Willock <crazcalm@gmail.com>
2022-07-31 20:46:15 +0000

    added a space

--- 01_07/solution/src/main.rs
+++ 01_07/solution/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let wanted_string = "a";
     let file_path = "file_with_lines";
     let contents: String = fs::read_to_string(file_path).expect("unable to open file");
+    
     for line in contents.lines() {
         if line.contains(wanted_string) {
             println!("{}", line);
