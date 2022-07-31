3cad31a8ecc1f39dcac7bd375741ebf6f01cfd21
Marcus Willock <crazcalm@gmail.com>
2022-07-31 19:46:01 +0000

    update solution 01_05

--- 01_05/solution/src/main.rs
+++ 01_05/solution/src/main.rs
@@ -2,9 +2,10 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() {
-    let contents: File = File::open("file_with_lines").unwrap();
-    let reader = io::BufReader::new(contents);
-    for line in reader.lines() {   
+    let file_path = "file_with_lines";
+    let file: File = File::open(file_path).unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
         println!("{}", line.unwrap());
     }
 }
